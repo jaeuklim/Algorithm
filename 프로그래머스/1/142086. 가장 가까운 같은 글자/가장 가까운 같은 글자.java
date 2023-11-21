b040717e2b5d7f57834c9fd6c87d43e13e735e9c
jaeuklim <81687612+jaeuklim@users.noreply.github.com>
2023-11-21 17:58:39 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 39.42 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -7,11 +7,11 @@ class Solution {
         tmp = s.split("");        
         
         for(int i =0; i<tmp.length; i++){
-            if(s.indexOf(tmp[i])==i)  //해당 문자열의 첫번째 위치가 탐색한 위취와 같은경우
+            if(s.indexOf(tmp[i])==i)  //해당 문자열의 첫번째 위치가 탐색한 위치와 같은경우
                 answer[i]=-1;               
-            else{
-                tmp2 = s.substring(0, i);               
-                answer[i]= i - tmp2.lastIndexOf(tmp[i]);
+            else{                     
+                tmp2 = s.substring(0, i);    //현재 값 위치를 반환하지 않기 위해 직전까지 문자열 slice.           
+                answer[i]= i - tmp2.lastIndexOf(tmp[i]);  //탐색하고자 하는 값의 마지막 등장 인덱스 위치를 찾음
             }           
         }
         
