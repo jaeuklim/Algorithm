e50382c3c2af7b22ffc4fae9daeaaed0816a26ed
jaeuklim <81687612+jaeuklim@users.noreply.github.com>
2023-12-20 15:59:09 +0900

    [level 1] Title: 체육복, Time: 0.50 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -2,62 +2,37 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int answer = n - lost.length;
+        int answer = n - lost.length; //도둑맞지 않은 현재 체육복 보유한 학생 수
+        int index;
         List<Integer> list = new ArrayList<>();
-        
-        for(int i=0; i<reserve.length; i++)
-            list.add(reserve[i]);
         
         Arrays.sort(lost);
         Arrays.sort(reserve);
         
+        for(int i=0; i<reserve.length; i++) //여벌 체육복 보유한 여분 리스트 생성
+            list.add(reserve[i]);
+        
         for(int i=0; i<lost.length; i++){
-            if(list.contains(lost[i])){
-                answer++;
-                list.remove(Integer.valueOf(lost[i]));
-                lost[i] = -1;
+            if(list.contains(lost[i])){     //본인이 도둑맞은 경우 본인 여분으로 대체
+                index = list.indexOf(lost[i]);
+                list.remove(index);         //여분 리스트에서 제외
+                lost[i] = -1;               
+                answer++;                   //옷을 구했으므로 보유 학생 수 증가
             }
         }
         
-        for(int i=0; i<lost.length; i++){
-            if(lost[i] == -1)
-                continue;
-            
-            if(list.contains(lost[i]-1)){
+        for(int i=0; i<lost.length; i++){            
+            if(list.contains(lost[i]-1)){       //앞 번호 학생에게 여벌이 있으면 대체
+                index = list.indexOf(lost[i]-1);
+                list.remove(index);
                 answer++;
-                list.remove(Integer.valueOf(lost[i]-1));
             }
-            else if(list.contains(lost[i]+1)){
+            else if(list.contains(lost[i]+1)){  // 뒷 번호 학생에게 여벌이 있으면 대체
+                index = list.indexOf(lost[i]+1);
+                list.remove(index);
                 answer++;
-                list.remove(Integer.valueOf(lost[i]+1));
             }
         }
-        
-        
-        
-        // for(int i=0; i<lost.length; i++){
-        //     for(int j=0; j<reserve.length; j++){
-        //         if(reserve[j] == lost[i]){
-        //             System.out.println(reserve[j]);
-        //             reserve[j] = -1;
-        //             lost[i] = -3;
-        //             answer++;
-        //             break;
-        //         }
-        //     }   
-        // }
-        
-//         for(int i=0; i<lost.length; i++){
-//             for(int j=0; j<reserve.length; j++){
-//                 if(reserve[j] == (lost[i] -1) || reserve[j] == (lost[i]+1)){
-//                     // System.out.println(reserve[j]);
-//                     reserve[j] = -1;
-//                     lost[i] = -3;
-//                     answer++;
-//                     break;
-//                 }
-//             }   
-//         }
         
         return answer;
     }
