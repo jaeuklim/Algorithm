09de046afadd766d4e0cf9b2854d8c0537965050
jaeuklim <81687612+jaeuklim@users.noreply.github.com>
2024-01-01 22:46:18 +0900

    [level unrated] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String num;
+        
+        num = Integer.toString(n);
+        for(char c : num.toCharArray())
+            answer += c - '0';
+
+        return answer;
+    }
+}
