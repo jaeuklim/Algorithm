b6cb8af8e46c0dee40c01ec3efd390e561c792d7
jaeuklim <81687612+jaeuklim@users.noreply.github.com>
2024-01-06 19:16:23 +0900

    [level unrated] Title: 모음 제거, Time: 1.73 ms, Memory: 69.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        String []aeiou = {"a","e","i","o","u"};
+        List<String> list = Arrays.asList(aeiou);
+        
+        for(int i = 0; i<my_string.length(); i++){
+            char tmp = my_string.charAt(i);
+            if(list.contains(Character.toString(tmp)))
+                continue;
+            else
+                answer+= Character.toString(tmp);                           
+        }
+        return answer;
+    }
+}
