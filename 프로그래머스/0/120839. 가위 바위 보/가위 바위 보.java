47bfb3d5a452c3c841cdad50c34b72e176d63430
jaeuklim <81687612+jaeuklim@users.noreply.github.com>
2024-01-17 09:41:28 +0900

    [level 0] Title: 가위 바위 보, Time: 2.77 ms, Memory: 87.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String rsp) {
+        String answer = "";
+        
+        for(char tmp : rsp.toCharArray()){
+            if(tmp == '2')
+                answer += '0';
+            else if(tmp == '0')
+                answer += '5';
+            else
+                answer += '2';                
+        }
+        return answer;
+    }
+}
