7d5cf7b2ab69c389c9cb302b6618764b5040c2d4
jaeuklim <81687612+jaeuklim@users.noreply.github.com>
2024-04-02 14:16:23 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.02 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
@@ -1,12 +1,13 @@
 class Solution {
     public int[] solution(int[] num_list, int n) {
         int[] answer = new int[num_list.length];
-        int index;
+//         int index;
   
         
         for(int i=0; i<answer.length; i++){
-            index = n % num_list.length;
-            answer[i] = num_list[index];
+            if(n==answer.length)
+                n=0;
+            answer[i] = num_list[n];
             n++;
             
         }
