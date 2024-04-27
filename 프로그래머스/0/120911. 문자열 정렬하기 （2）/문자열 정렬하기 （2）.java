fbb97d5e04d36b511c3a02af28b46f3a97a60b9f
jaeuklim <81687612+jaeuklim@users.noreply.github.com>
2024-04-27 22:57:28 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.55 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string) {
+        String answer = my_string.toLowerCase();
+        
+        char []tmp = answer.toCharArray();
+        
+        Arrays.sort(tmp);
+        
+        answer = new String(tmp);
+        
+        return answer;
+    }
+}
