4ae2fc30fb10dd76ed44d28a2b61a26d7cabbc84
jaeuklim <81687612+jaeuklim@users.noreply.github.com>
2024-06-01 22:08:28 +0900

    [level 0] Title: 글자 지우기, Time: 12.80 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string, int[] indices) {
+        String answer = "";
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i=0; i<indices.length; i++)
+            list.add(indices[i]);
+        
+        for(int i=0; i<my_string.length(); i++)
+            if(!list.contains(i))
+                answer += my_string.charAt(i);
+        
+        return answer;
+    }
+}
