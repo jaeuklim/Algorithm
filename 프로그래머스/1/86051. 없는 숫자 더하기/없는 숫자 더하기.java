69e1869e211889abbbf47d7dae6f2a4031fddf4c
jaeuklim <81687612+jaeuklim@users.noreply.github.com>
2024-07-01 18:13:21 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.06 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i=0; i<numbers.length; i++)
+            list.add(numbers[i]);
+        
+        for(int i=0; i<10; i++)
+            if(!list.contains(i))
+                answer+=i;
+        
+        return answer;
+    }
+}
