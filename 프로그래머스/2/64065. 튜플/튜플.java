82469e71d004613428eeddeb937ae52d13d84e3f
jaeuklim <81687612+jaeuklim@users.noreply.github.com>
2024-09-05 19:41:14 +0900

    [level 2] Title: 튜플, Time: 160.95 ms, Memory: 139 MB -BaekjoonHub

---
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String s) {
+        String[] tmp = s.split("[{,}]");
+        Map<String, Integer> map = new HashMap<>();
+              
+        
+        for(String str : tmp){
+            if(str.matches("\\d+")){
+                map.put(str, map.getOrDefault(str,0)+1);
+            }                  
+        }
+        
+        int[] answer = new int[map.size()];
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i : map.values())
+            list.add(i);
+        
+            
+        
+        Collections.sort(list, Collections.reverseOrder());
+        
+        for (int i = 0; i < answer.length; i++) {
+            int freq = list.get(i);
+            
+            for (Map.Entry<String, Integer> entry : map.entrySet()) {
+                if (entry.getValue() == freq) {
+                    answer[i] = Integer.parseInt(entry.getKey());
+                    break;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
