6790be4b54825ec8b10db162ac3633fdb7141096
jaeuklim <81687612+jaeuklim@users.noreply.github.com>
2024-09-13 18:51:13 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 11.58 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -7,55 +7,41 @@ class Solution {
         int union = 0;
         List<String> list1 = new ArrayList<>();
         List<String> list2 = new ArrayList<>();
-        List<String> interlist = new ArrayList<>();
-        List<String> unionlist = new ArrayList<>();
         
         str1 = str1.toUpperCase();
         str2 = str2.toUpperCase();
         
         for(int i=0; i<str1.length()-1; i++){
-            // char a = str1.charAt(i);
-            // char b = str2.charAt()
-            if(Character.isLetter(str1.charAt(i)) && Character.isLetter(str1.charAt(i+1))){
+            if(str1.charAt(i) >= 'A' && str1.charAt(i) <= 'Z'){
+                if(str1.charAt(i+1) >= 'A' && str1.charAt(i+1) <= 'Z'){
                     list1.add(""+str1.charAt(i)+str1.charAt(i+1));
-
+                }
             }
         }
         
         for(int i=0; i<str2.length()-1; i++){
-            if(Character.isLetter(str2.charAt(i)) && Character.isLetter(str2.charAt(i+1))){
-                list2.add(""+str2.charAt(i)+str2.charAt(i+1));
-                // if(str2.charAt(i+1) >= 'A' && str2.charAt(i+1) <= 'Z'){
-                //     list2.add(""+str2.charAt(i)+str2.charAt(i+1));
-                // }   
+            if(str2.charAt(i) >= 'A' && str2.charAt(i) <= 'Z'){
+                if(str2.charAt(i+1) >= 'A' && str2.charAt(i+1) <= 'Z'){
+                    list2.add(""+str2.charAt(i)+str2.charAt(i+1));
+                }   
             }
         }
-        
-        Collections.sort(list1);
-        Collections.sort(list2);
         
         List<String> tmplist = new ArrayList<>(list2);
         
         for(String s : list1){
-            if(list2.remove(s)){
-                interlist.add(s);
-            }
-            unionlist.add(s);
+            if(tmplist.contains(s)){
+                inter++;
+                tmplist.remove(s);
+            }   
         }
         
-        for(String s : list2){
-            unionlist.add(s);
-        }
-        
-        union  += tmplist.size();
+        union  = list1.size() + list2.size() - inter;
             
-        // if(inter==0 || union == 0)
-        //     return 65536;
-        
-        if(unionlist.size()==0)
+        if(union==0)
             return 65536;
-        else
-            answer = (int)(((double)interlist.size()/(double)unionlist.size())*65536);
+        
+        answer = (int)(((double)inter/(double)union)*65536);
         
         return answer;
     }
