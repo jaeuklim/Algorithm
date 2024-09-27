726605b3db47c617fe3c73a84fa2c50387547993
jaeuklim <81687612+jaeuklim@users.noreply.github.com>
2024-09-27 18:36:27 +0900

    [level 2] Title: [3차] 압축, Time: 30.14 ms, Memory: 85.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String msg) {
+        List<String> list = new ArrayList<>();
+        List<Integer> answerlist = new ArrayList<>();
+        String wc = "";
+        String lastwc = "";
+        
+        for(int i=0; i<26; i++)
+            list.add(Character.toString('A'+i)); 
+        
+        for(int i=0; i<msg.length(); i++){
+            wc += msg.charAt(i);
+            // index = i;            
+            
+            if(list.contains(wc)){
+                lastwc += msg.charAt(i);
+                continue;
+            }
+            else{
+                answerlist.add(list.indexOf(lastwc)+1);
+                list.add(wc);
+                wc = "";
+                lastwc = "";
+                i--;
+            }            
+        }
+        
+        answerlist.add(list.indexOf(wc)+1);
+        
+        int[] answer = new int[answerlist.size()];
+        
+        for(int i=0; i<answer.length; i++)
+            answer[i] = answerlist.get(i);           
+        
+        return answer;
+    }
+}
