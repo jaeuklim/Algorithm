3048244d2754a39d5cddc4532e5972279de0afdc
jaeuklim <81687612+jaeuklim@users.noreply.github.com>
2024-10-04 20:00:16 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.05 ms, Memory: 80.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 1;
+        List<Integer> list = new ArrayList<>();
+        
+        int mult = b / mult(b,a);
+        
+        while(mult>1){
+            if(mult%2==0)
+                mult=mult/2;
+            else if(mult%5==0)
+                mult=mult/5;
+            else
+                return 2;
+        }
+        
+        return answer;
+    }
+    
+    public int mult(int a, int b){
+        if(a%b==0)
+            return b;
+        
+        return mult(b, a%b);
+    }
+}
