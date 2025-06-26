db64755d6f64fee6c68793d990fa33ae9df79df7
jaeuklim <81687612+jaeuklim@users.noreply.github.com>
2025-06-26 15:18:26 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 80.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[2];        
+        int i = 0;
+        int j = 0;
+        
+        for(int k=0; k<num_list.length; k++){
+            if(num_list[k] % 2 == 0)
+                i++;
+            else
+                j++;
+        }
+        
+        answer[0] = i;
+        answer[1] = j;
+        
+        return answer;
+    }
+}
