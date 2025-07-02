efe7dff3d01197625850763175a14d75b8fc052f
jaeuklim <81687612+jaeuklim@users.noreply.github.com>
2025-07-02 18:45:36 +0900

    [level 0] Title: 편지, Time: 0.03 ms, Memory: 83.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120898. 편지/편지.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+        int size = message.length();
+        
+        answer = size * 2;
+        
+        return answer;
+    }
+}
