e01738ccded4c7a568739942724e605ff87dd41f
jaeuklim <81687612+jaeuklim@users.noreply.github.com>
2025-08-26 16:03:00 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.03 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(String str1, String str2) {
         int answer = 0;
-        boolean tmp = str1.contains(str2); // str1문자열 속에 str2가 존재하면 true 를 반환 
+        boolean tmp = str1.contains(str2);
 
         if(tmp)
             answer = 1;
