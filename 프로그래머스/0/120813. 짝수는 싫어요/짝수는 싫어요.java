09f58c89ee6d19c125a699bea893a1004ed819a0
jaeuklim <81687612+jaeuklim@users.noreply.github.com>
2025-09-30 19:50:25 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.07 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -11,7 +11,7 @@ class Solution {
         
         int[] answer = new int [list.size()];
         
-        for(int i= 0; i<list.size(); i++)
+        for(int i=0; i<list.size(); i++)
             answer[i]=list.get(i);
         
         return answer;
