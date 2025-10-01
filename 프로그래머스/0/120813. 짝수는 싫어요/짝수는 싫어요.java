c6b860d3ef1ac2462d2a5a61fc50cf794347df65
jaeuklim <81687612+jaeuklim@users.noreply.github.com>
2025-10-01 21:55:06 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.07 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -4,7 +4,7 @@ class Solution {
     public int[] solution(int n) {
         List<Integer> list = new ArrayList<>();
         
-        for(int i= 1; i<=n; i++){
+        for(int i=1; i<=n; i++){
             if(i%2!=0)
                 list.add(i);
         }
