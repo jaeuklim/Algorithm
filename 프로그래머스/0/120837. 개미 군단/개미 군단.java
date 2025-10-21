b301add8e478cf21923601474342b4f06f69798b
jaeuklim <81687612+jaeuklim@users.noreply.github.com>
2025-10-21 22:58:48 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -3,7 +3,7 @@ class Solution {
         int answer = 0;
         
         answer += hp / 5;
-        hp = hp%5;
+        hp = hp % 5;
         answer += hp / 3;
         hp = hp%3;
         answer += hp;
